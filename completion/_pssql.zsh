513f7ab5ffc2603735fbcba273b31bf20eb6d480
Branco <fausto.branco@kuehne-nagel.com>
2026-02-06 15:04:49 +0000

    [FEAT] - 2026-02-06 15:04:49

--- completion/_pssql.zsh
+++ completion/_pssql.zsh
@@ -2,8 +2,7 @@
 
 _pssql() {
     local -a servers
-    # O uso de (@f) garante que cada linha do output do Go seja um item da lista
-    servers=("${(@f)$((PSSQL_COMPLETION_MODE=true pssql 2>/dev/null))}")
+    servers=(${(f)"$(PSSQL_COMPLETION_MODE=true pssql 2>/dev/null)"})
 
     _arguments \
         '--connect[Server name]:server:compadd -a servers' \
